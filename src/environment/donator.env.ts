970505a74242a1f93dd3fec679faf33b8ecc3dae
Agniv Ghosh <agnivg157@gmail.com>
2021-06-12 19:44:30 +0530

    initial commit

---
+++ src/environment/donator.env.ts
@@ -0,0 +1,9 @@
+import {Environment} from "./env";
+
+export const DonatorEnvironment : Environment = {
+    db_url : 'mongodb+srv://admin:admin123@cluster0.yk9sl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+ //jwt_secret : 'token'
+ jwt_secret: 'donator'
+
+};
---
+++ src/environment/org.env.ts
@@ -0,0 +1,7 @@
+import {Environment} from "./env";
+
+export const OrgEnvironment : Environment = {
+    db_url : 'mongodb+srv://admin:admin123@cluster0.yk9sl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+   // jwt_secret : 'token'
+    jwt_secret : 'organization'
+};
