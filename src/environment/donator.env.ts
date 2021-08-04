936f45cc0915f006a499c8d3544e35ca9ba070cd
Agniv Ghosh <agnivg157@gmail.com>
2021-08-04 11:20:02 +0530

    commit

--- src/environment/donator.env.ts
+++ src/environment/donator.env.ts
@@ -2,7 +2,7 @@ import {Environment} from "./env";
 
 export const DonatorEnvironment : Environment = {
     //db_url : 'mongodb+srv://admin:admin123@cluster0.yk9sl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-     db_url : 'mongodb://localhost:27017/Charity',
+     db_url : 'mongodb://localhost:27017/CharityDb',
     //jwt_secret : 'token'
     jwt_secret: 'donator'
 
--- src/environment/org.env.ts
+++ src/environment/org.env.ts
@@ -3,6 +3,6 @@ import {Environment} from "./env";
 export const OrgEnvironment : Environment = {
     //db_url : 'mongodb+srv://admin:admin123@cluster0.yk9sl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     // jwt_secret : 'token'
-    db_url:'mongodb://localhost:27017/Charity',
+    db_url:'mongodb://localhost:27017/CharityDb',
     jwt_secret : 'organization'
 };
