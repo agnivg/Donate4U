1c211e9bd2d2dc1c20de67093a07a1249004f0e1
agnivg <agnivg157@gmail.com>
2021-10-09 02:30:43 +0530

    changed sendgrid to nodemailer

--- src/server.ts
+++ src/server.ts
@@ -35,8 +35,9 @@ export class Server {
     }
 
     connectMongoDb() {
-        const databaseUrl = "mongodb+srv://adminuser:useradmin@charitydb.fivwx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-        mongoose.connect(process.env.MONGODB_URI || databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false}).then(() => {
+        //const databaseUrl = "mongodb+srv://adminuser:useradmin@charitydb.fivwx.mongodb.net/CharityDb?retryWrites=true&w=majority";
+        const databaseUrl='mongodb://localhost:27017/CharityDb'
+        mongoose.connect(databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false}).then(() => {
             console.log('connected to database');
         });
     }
